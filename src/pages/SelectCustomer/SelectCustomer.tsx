eb171aed2c40af5651bf32ddbd96d82affda3eaa
中野 辰洋 <91533094+nakano-ta@users.noreply.github.com>
2024-02-21 12:12:45 +0900

    Revert "SelectCustomer画面にMenubarとヘッダーを追加"

--- src/pages/SelectCustomer/SelectCustomer.tsx
+++ src/pages/SelectCustomer/SelectCustomer.tsx
@@ -37,8 +37,6 @@ import { createApi } from "@/apis/createApi";
 import { useForm } from "react-hook-form";
 import { loginInformation } from "@/types";
 import { useNavigate, Link as ReactRouterLink } from "react-router-dom";
-import MenuBar from "@/componets/MenuBar/MenuBar";
-import { Header } from "@/componets/Header";
 
 export const SelectCustomer: FC = () => {
   const navigate = useNavigate();
@@ -61,75 +59,71 @@ export const SelectCustomer: FC = () => {
   };
 
   return (
-    <Container maxW="none" p={0} h={"100%"}>
-      <MenuBar />
-      <Header customer="TODO 表示するだけの仮の値" />
-      <Container alignItems={"center"} pt={"10rem"}>
-        <VStack gap={"2rem"}>
-          <Text fontSize={"1.5rem"} fontWeight={"bold"}>
-            カルテ作成モードの選択
-          </Text>
-          <form onSubmit={handleSubmit(onSubmit)}>
-            <VStack gap={"2rem"}>
-              <Box width={"25rem"}>
-                <Text>会員番号</Text>
-                <Input
-                  type="text"
-                  {...register("cardNumber", {
-                    required: "This is required",
-                  })}
-                />
-              </Box>
-              <Box width={"25rem"}>
-                <Text>お客様の生年月日</Text>
-                <Input
-                  type="date"
-                  {...register("birthday", {
-                    required: "This is required",
-                  })}
-                />
-              </Box>
-              <Button
-                mt={4}
-                bg={"brandOrange.500"}
-                color={"white"}
-                isLoading={isSubmitting}
-                type="submit"
-                isDisabled={!isValid}
-                pl={"4rem"}
-                pr={"4rem"}
-                borderRadius={"5rem"}
-                height={"3rem"}
-              >
-                既存のお客様で作成
-              </Button>
-            </VStack>
-          </form>
-          <Link as={ReactRouterLink} to="/customers/new-karte">
-            <Box position="relative" padding="10" width={"100%"}>
-              <Divider
-                borderWidth={"1px"}
-                borderColor={" rgba(105, 145, 172, 1)"}
+    <Container alignItems={"center"} pt={"10rem"}>
+      <VStack gap={"2rem"}>
+        <Text fontSize={"1.5rem"} fontWeight={"bold"}>
+          カルテ作成モードの選択
+        </Text>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <VStack gap={"2rem"}>
+            <Box width={"25rem"}>
+              <Text>会員番号</Text>
+              <Input
+                type="text"
+                {...register("cardNumber", {
+                  required: "This is required",
+                })}
               />
-              <AbsoluteCenter bg="white" px="4" width={"5rem"}>
-                または
-              </AbsoluteCenter>
+            </Box>
+            <Box width={"25rem"}>
+              <Text>お客様の生年月日</Text>
+              <Input
+                type="date"
+                {...register("birthday", {
+                  required: "This is required",
+                })}
+              />
             </Box>
             <Button
               mt={4}
               bg={"brandOrange.500"}
               color={"white"}
+              isLoading={isSubmitting}
               type="submit"
+              isDisabled={!isValid}
               pl={"4rem"}
               pr={"4rem"}
               borderRadius={"5rem"}
               height={"3rem"}
             >
-              新規のお客様で作成
+              既存のお客様で作成
             </Button>
-          </Link>
-        </VStack>
-      </Container>
+          </VStack>
+        </form>
+        <Link as={ReactRouterLink} to="/customers/new-karte">
+          <Box position="relative" padding="10" width={"100%"}>
+            <Divider
+              borderWidth={"1px"}
+              borderColor={" rgba(105, 145, 172, 1)"}
+            />
+            <AbsoluteCenter bg="white" px="4" width={"5rem"}>
+              または
+            </AbsoluteCenter>
+          </Box>
+          <Button
+            mt={4}
+            bg={"brandOrange.500"}
+            color={"white"}
+            type="submit"
+            pl={"4rem"}
+            pr={"4rem"}
+            borderRadius={"5rem"}
+            height={"3rem"}
+          >
+            新規のお客様で作成
+          </Button>
+        </Link>
+      </VStack>
     </Container>
   );
 };
