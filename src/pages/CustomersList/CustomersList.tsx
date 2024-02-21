a281b78124cf97330610c102c16648d545af1e7e
Tatsuhiro Nakano <t-nakano@pca.co.jp>
2024-02-21 12:10:41 +0900

    他の画面にもNavbarとHeaderを追加

--- src/pages/CustomersList/CustomersList.tsx
+++ src/pages/CustomersList/CustomersList.tsx
@@ -37,25 +37,31 @@ import { Header } from "@/componets/Header";
 
 const CustomersList = () => {
   return (
-    <Container alignItems={"center"} pt={"10rem"}>
-      <VStack gap={"2rem"}>
-        <Text fontSize={"1.5rem"} fontWeight={"bold"}>
-          CustomersList
-        </Text>
-        <Link as={ReactRouterLink} to="/stylist/customers/1">
-          <Button
-            mt={4}
-            bg={"brandOrange.500"}
-            color={"white"}
-            pl={"4rem"}
-            pr={"4rem"}
-            borderRadius={"5rem"}
-            height={"3rem"}
-          >
-            Customer
-          </Button>
-        </Link>
-      </VStack>
+    <Container maxW="none" p={0} h={"100%"}>
+      <MenuBar />
+      <Container maxW="none" p={"2rem"} bg={"brandGray.500"} h={"100%"}>
+        <VStack alignItems={"start"} gap={"1rem"}>
+          <Header customer="TODO dummy" />
+        </VStack>
+        <VStack gap={"2rem"}>
+          <Text fontSize={"1.5rem"} fontWeight={"bold"}>
+            CustomersList
+          </Text>
+          <Link as={ReactRouterLink} to="/stylist/customers/1">
+            <Button
+              mt={4}
+              bg={"brandOrange.500"}
+              color={"white"}
+              pl={"4rem"}
+              pr={"4rem"}
+              borderRadius={"5rem"}
+              height={"3rem"}
+            >
+              Customer
+            </Button>
+          </Link>
+        </VStack>
+      </Container>
     </Container>
   );
 };
--- src/pages/MainMenu/MainMenu.tsx
+++ src/pages/MainMenu/MainMenu.tsx
@@ -37,38 +37,41 @@ import { Header } from "@/componets/Header";
 
 const MainMenu = () => {
   return (
-    <Container alignItems={"center"} pt={"10rem"}>
-      <VStack gap={"2rem"}>
-        <Text fontSize={"1.5rem"} fontWeight={"bold"}>
-          利用者選択
-        </Text>
-        <Link as={ReactRouterLink} to="/customers/select-customer">
-          <Button
-            mt={4}
-            bg={"brandOrange.500"}
-            color={"white"}
-            pl={"4rem"}
-            pr={"4rem"}
-            borderRadius={"5rem"}
-            height={"3rem"}
-          >
-            お客様
-          </Button>
-        </Link>
-        <Link as={ReactRouterLink} to="/stylist/customers/List">
-          <Button
-            mt={4}
-            bg={"brandOrange.500"}
-            color={"white"}
-            pl={"4rem"}
-            pr={"4rem"}
-            borderRadius={"5rem"}
-            height={"3rem"}
-          >
-            美容師
-          </Button>
-        </Link>
-      </VStack>
+    <Container maxW="none" p={0} h={"100%"}>
+      <MenuBar />
+      <Container maxW="none" p={"2rem"} bg={"brandGray.500"} h={"100%"}>
+        <VStack gap={"2rem"}>
+          <Text fontSize={"1.5rem"} fontWeight={"bold"}>
+            利用者選択
+          </Text>
+          <Link as={ReactRouterLink} to="/customers/select-customer">
+            <Button
+              mt={4}
+              bg={"brandOrange.500"}
+              color={"white"}
+              pl={"4rem"}
+              pr={"4rem"}
+              borderRadius={"5rem"}
+              height={"3rem"}
+            >
+              お客様
+            </Button>
+          </Link>
+          <Link as={ReactRouterLink} to="/stylist/customers/List">
+            <Button
+              mt={4}
+              bg={"brandOrange.500"}
+              color={"white"}
+              pl={"4rem"}
+              pr={"4rem"}
+              borderRadius={"5rem"}
+              height={"3rem"}
+            >
+              美容師
+            </Button>
+          </Link>
+        </VStack>
+      </Container>
     </Container>
   );
 };
